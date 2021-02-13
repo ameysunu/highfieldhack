6100b9d3b1405e141ac7d27dae0def81ba20aee1
Amey Sunu <ameysunu00719@gmail.com>
2021-02-13 23:15:52 +0300

    cupertino icons added

--- lib/homewidget.dart
+++ lib/homewidget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hackpsu/quiz.dart';
 import 'package:hackpsu/split.dart';
@@ -24,42 +25,43 @@ class _HomeWidgetState extends State<HomeWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: HexColor('#FFC1B2'),
+          backgroundColor: HexColor('#363636'),
           type: BottomNavigationBarType.fixed,
           currentIndex: _selectedIndex,
           showSelectedLabels: true,
           items: [
             BottomNavigationBarItem(
-              icon: new Icon(Icons.library_books, color: HexColor('#545E78')),
+              icon: new Icon(CupertinoIcons.book, color: HexColor('#FFFFFF')),
               title: new Text(
                 'Tutorials',
                 style: TextStyle(
-                    color: HexColor('#545E78'), fontFamily: 'Roboto Medium'),
+                    color: HexColor('#FFFFFF'), fontFamily: 'Roboto Medium'),
               ),
             ),
             BottomNavigationBarItem(
-              icon:
-                  new Icon(Icons.horizontal_split, color: HexColor('#545E78')),
+              icon: new Icon(CupertinoIcons.square_split_1x2,
+                  color: HexColor('#FFFFFF')),
               title: new Text(
                 'Split',
                 style: TextStyle(
-                    color: HexColor('#545E78'), fontFamily: 'Roboto Medium'),
+                    color: HexColor('#FFFFFF'), fontFamily: 'Roboto Medium'),
               ),
             ),
             BottomNavigationBarItem(
-              icon: new Icon(Icons.help_center, color: HexColor('#545E78')),
+              icon: new Icon(CupertinoIcons.question_circle,
+                  color: HexColor('#FFFFFF')),
               title: new Text(
                 'Quiz',
                 style: TextStyle(
-                    color: HexColor('#545E78'), fontFamily: 'Roboto Medium'),
+                    color: HexColor('#FFFFFF'), fontFamily: 'Roboto Medium'),
               ),
             ),
             BottomNavigationBarItem(
-              icon: new Icon(Icons.person, color: HexColor('#545E78')),
+              icon: new Icon(CupertinoIcons.person, color: HexColor('#FFFFFF')),
               title: new Text(
                 'User',
                 style: TextStyle(
-                    color: HexColor('#545E78'), fontFamily: 'Roboto Medium'),
+                    color: HexColor('#FFFFFF'), fontFamily: 'Roboto Medium'),
               ),
             ),
           ],
